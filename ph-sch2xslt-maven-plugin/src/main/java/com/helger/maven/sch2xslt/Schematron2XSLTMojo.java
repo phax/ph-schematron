3e3048168dec795bad89f396e06b272fd2985cb1
Philip Helger <philip@helger.com>
2017-08-01 20:51:00 +0200

    Replaced constants

--- ph-sch2xslt-maven-plugin/src/main/java/com/helger/maven/sch2xslt/Schematron2XSLTMojo.java
+++ ph-sch2xslt-maven-plugin/src/main/java/com/helger/maven/sch2xslt/Schematron2XSLTMojo.java
@@ -21,6 +21,7 @@ import java.io.OutputStream;
 import java.util.Locale;
 
 import javax.annotation.Nonnull;
+import javax.xml.XMLConstants;
 import javax.xml.transform.ErrorListener;
 
 import org.apache.maven.plugin.AbstractMojo;
@@ -41,7 +42,6 @@ import com.helger.schematron.svrl.CSVRL;
 import com.helger.schematron.xslt.ISchematronXSLTBasedProvider;
 import com.helger.schematron.xslt.SCHTransformerCustomizer;
 import com.helger.schematron.xslt.SchematronResourceSCHCache;
-import com.helger.xml.CXML;
 import com.helger.xml.XMLHelper;
 import com.helger.xml.namespace.MapBasedNamespaceContext;
 import com.helger.xml.serialize.write.XMLWriter;
@@ -309,7 +309,7 @@ public final class Schematron2XSLTMojo extends AbstractMojo
               final MapBasedNamespaceContext aNSContext = new MapBasedNamespaceContext ().addMapping ("svrl",
                                                                                                       CSVRL.SVRL_NAMESPACE_URI);
               // Add all namespaces from XSLT document root
-              final String sNSPrefix = CXML.XML_ATTR_XMLNS + ":";
+              final String sNSPrefix = XMLConstants.XMLNS_ATTRIBUTE + ":";
               XMLHelper.forAllAttributes (aXsltProvider.getXSLTDocument ().getDocumentElement (),
                                           (sAttrName, sAttrValue) -> {
                                             if (sAttrName.startsWith (sNSPrefix))
