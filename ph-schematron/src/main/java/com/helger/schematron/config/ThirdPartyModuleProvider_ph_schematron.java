ade117cfd004d4a32b024bb248de90ed07055534
Philip Helger <philip@helger.com>
2019-07-12 18:38:46 +0200

    Updated to Saxon-HE 9.9.1-4

--- ph-schematron/src/main/java/com/helger/schematron/config/ThirdPartyModuleProvider_ph_schematron.java
+++ ph-schematron/src/main/java/com/helger/schematron/config/ThirdPartyModuleProvider_ph_schematron.java
@@ -37,7 +37,7 @@ public final class ThirdPartyModuleProvider_ph_schematron implements IThirdParty
   public static final IThirdPartyModule SAXON_HE = new ThirdPartyModule ("Saxon HE",
                                                                          "Saxonica Limited",
                                                                          ELicense.MPL20,
-                                                                         new Version (9, 9, 1, "3 HE"),
+                                                                         new Version (9, 9, 1, "4 HE"),
                                                                          "http://saxon.sourceforge.net/");
 
   @Nullable
