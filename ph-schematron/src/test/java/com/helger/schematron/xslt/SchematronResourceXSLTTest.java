8a69238e2355754b696076ff9d6a2a8cefe15ee8
Philip Helger <philip@helger.com>
2020-09-28 14:29:16 +0200

    Test fix

--- ph-schematron/src/test/java/com/helger/schematron/xslt/SchematronResourceXSLTTest.java
+++ ph-schematron/src/test/java/com/helger/schematron/xslt/SchematronResourceXSLTTest.java
@@ -1,73 +1,78 @@
 package com.helger.schematron.xslt;
 
-import com.helger.commons.io.resource.ClassPathResource;
-import com.helger.commons.io.stream.StringInputStream;
+import static org.junit.Assert.assertNotNull;
+
+import java.nio.charset.StandardCharsets;
+
 import org.junit.Assert;
 import org.junit.Test;
 import org.w3c.dom.Document;
 
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
+import com.helger.commons.io.resource.ClassPathResource;
+import com.helger.commons.io.stream.NonBlockingByteArrayInputStream;
+import com.helger.commons.io.stream.StreamHelper;
 
-import static org.junit.Assert.assertNotNull;
+/**
+ * Test class for class {@link SchematronResourceXSLT}.
+ *
+ * @author Philip Helger
+ */
+public final class SchematronResourceXSLTTest
+{
+  private static final ClassPathResource VALID_XSLT_SCHEMATRON = new ClassPathResource ("/test-xslt/valid01.xslt");
+  private static final ClassPathResource VALID_XMLINSTANCE = new ClassPathResource ("/test-xml/valid01.xml");
 
-public class SchematronResourceXSLTTest {
-    private static final String VALID_XSLT_SCHEMATRON = "/test-xslt/valid01.xslt";
-    private static final String VALID_XMLINSTANCE = "/test-xml/valid01.xml";
+  @Test
+  public void testFromUrl () throws Exception
+  {
+    final SchematronResourceXSLT sch = SchematronResourceXSLT.fromURL (VALID_XSLT_SCHEMATRON.getAsURL ());
+    Assert.assertTrue ("invalid schematron", sch.isValidSchematron ());
 
-    @Test
-    public void testFromUrl() throws Exception {
-        final URL schUrl = getClass().getResource(VALID_XSLT_SCHEMATRON);
-        final SchematronResourceXSLT sch = SchematronResourceXSLT.fromURL(schUrl);
-        Assert.assertTrue("invalid schematron", sch.isValidSchematron());
+    final Document aDoc = sch.applySchematronValidation (VALID_XMLINSTANCE);
+    assertNotNull (aDoc);
+  }
 
-        final Document aDoc = sch.applySchematronValidation(new ClassPathResource(VALID_XMLINSTANCE));
-        assertNotNull(aDoc);
-    }
+  @Test
+  public void testFromStringUrl () throws Exception
+  {
+    final SchematronResourceXSLT sch = SchematronResourceXSLT.fromURL (VALID_XSLT_SCHEMATRON.getAsURL ().toExternalForm ());
+    Assert.assertTrue ("invalid schematron", sch.isValidSchematron ());
 
-    @Test
-    public void testFromStringUrl() throws Exception {
-        final URL schUrl = getClass().getResource(VALID_XSLT_SCHEMATRON);
-        final SchematronResourceXSLT sch = SchematronResourceXSLT.fromURL(schUrl.toString());
-        Assert.assertTrue("invalid schematron", sch.isValidSchematron());
+    final Document aDoc = sch.applySchematronValidation (VALID_XMLINSTANCE);
+    assertNotNull (aDoc);
+  }
 
-        final Document aDoc = sch.applySchematronValidation(new ClassPathResource(VALID_XMLINSTANCE));
-        assertNotNull(aDoc);
-    }
+  @Test
+  public void testFromInputStream () throws Exception
+  {
+    final byte [] aPayload = StreamHelper.getAllBytes (VALID_XSLT_SCHEMATRON);
+    final SchematronResourceXSLT sch = SchematronResourceXSLT.fromInputStream (new NonBlockingByteArrayInputStream (aPayload));
+    Assert.assertTrue ("invalid schematron", sch.isValidSchematron ());
 
-    @Test
-    public void testFromInputStream() throws Exception {
-        final String schStr = loadSchString(VALID_XSLT_SCHEMATRON);
-        final SchematronResourceXSLT sch = SchematronResourceXSLT.fromInputStream(new StringInputStream(schStr, StandardCharsets.UTF_8));
-        Assert.assertTrue("invalid schematron", sch.isValidSchematron());
+    final Document aDoc = sch.applySchematronValidation (VALID_XMLINSTANCE);
+    assertNotNull (aDoc);
+  }
 
-        final Document aDoc = sch.applySchematronValidation(new ClassPathResource(VALID_XMLINSTANCE));
-        assertNotNull(aDoc);
-    }
+  @Test
+  public void testFromByteArray () throws Exception
+  {
+    final byte [] aPayload = StreamHelper.getAllBytes (VALID_XSLT_SCHEMATRON);
+    final SchematronResourceXSLT sch = SchematronResourceXSLT.fromByteArray (aPayload);
+    Assert.assertTrue ("invalid schematron", sch.isValidSchematron ());
 
-    @Test
-    public void testFromByteArray() throws Exception {
-        final String schStr = loadSchString(VALID_XSLT_SCHEMATRON);
-        final byte[] ba = schStr.getBytes();
-        final SchematronResourceXSLT sch = SchematronResourceXSLT.fromByteArray(ba);
-        Assert.assertTrue("invalid schematron", sch.isValidSchematron());
+    final Document aDoc = sch.applySchematronValidation (VALID_XMLINSTANCE);
+    assertNotNull (aDoc);
+  }
 
-        final Document aDoc = sch.applySchematronValidation(new ClassPathResource(VALID_XMLINSTANCE));
-        assertNotNull(aDoc);
-    }
+  @Test
+  public void testFromString () throws Exception
+  {
+    final byte [] aPayload = StreamHelper.getAllBytes (VALID_XSLT_SCHEMATRON);
+    final SchematronResourceXSLT sch = SchematronResourceXSLT.fromString (new String (aPayload, StandardCharsets.UTF_8),
+                                                                          StandardCharsets.UTF_8);
+    Assert.assertTrue ("invalid schematron", sch.isValidSchematron ());
 
-    @Test
-    public void testFromString() throws Exception {
-        final String schStr = loadSchString(VALID_XSLT_SCHEMATRON);
-        final SchematronResourceXSLT sch = SchematronResourceXSLT.fromString(schStr, StandardCharsets.UTF_8);
-        Assert.assertTrue("invalid schematron", sch.isValidSchematron());
-
-        final Document aDoc = sch.applySchematronValidation(new ClassPathResource(VALID_XMLINSTANCE));
-        assertNotNull(aDoc);
-    }
-
-    private String loadSchString(final String path) throws Exception {
-        return new String(Files.readAllBytes(new ClassPathResource(path).getAsFile().toPath()));
-    }
+    final Document aDoc = sch.applySchematronValidation (VALID_XMLINSTANCE);
+    assertNotNull (aDoc);
+  }
 }
