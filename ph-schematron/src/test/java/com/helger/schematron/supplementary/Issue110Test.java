6b78c01ed451d815f58f511c3fa23908ba45e0cd
Philip Helger <philip@helger.com>
2020-11-19 11:57:15 +0100

    Ignoring test

--- ph-schematron/src/test/java/com/helger/schematron/supplementary/Issue110Test.java
+++ ph-schematron/src/test/java/com/helger/schematron/supplementary/Issue110Test.java
@@ -3,17 +3,21 @@ package com.helger.schematron.supplementary;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import com.helger.commons.io.resource.ClassPathResource;
+import com.helger.commons.io.resourceresolver.DefaultResourceResolver;
 import com.helger.schematron.ISchematronResource;
 import com.helger.schematron.xslt.SchematronResourceSCH;
 
 public final class Issue110Test
 {
   @Test
+  @Ignore ("Relies on Saxon bug")
   public void testIncludedFilesNotDeleted () throws Exception
   {
+    DefaultResourceResolver.setDebugResolve (true);
     final String sPath = "issues/github110/";
     final ISchematronResource aSV = SchematronResourceSCH.fromClassPath (sPath + "ATGOV-UBL-T10.sch");
     aSV.applySchematronValidation (new ClassPathResource (sPath + "test.xml"));
