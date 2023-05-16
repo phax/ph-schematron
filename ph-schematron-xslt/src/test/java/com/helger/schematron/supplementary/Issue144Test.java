6171ed9d864258d7eb05ed4687e01a58437e5329
Philip Helger <philip@helger.com>
2023-05-16 23:00:45 +0200

    Added another test

--- ph-schematron-xslt/src/test/java/com/helger/schematron/supplementary/Issue144Test.java
+++ ph-schematron-xslt/src/test/java/com/helger/schematron/supplementary/Issue144Test.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 import org.junit.Test;
 import org.slf4j.Logger;
@@ -54,9 +55,56 @@ public final class Issue144Test
     final SchematronOutputType svrl = aResSCH.applySchematronValidationToSVRL (new StringStreamSource (xmlFile));
     assertNotNull (svrl);
 
-    LOGGER.info (new SVRLMarshaller ().setFormattedOutput (true).getAsString (svrl));
+    if (false)
+      LOGGER.info (new SVRLMarshaller ().setFormattedOutput (true).getAsString (svrl));
 
     final ICommonsList <AbstractSVRLMessage> errs = SVRLHelper.getAllFailedAssertionsAndSuccessfulReports (svrl);
     assertEquals (1, errs.size ());
+  }
+
+  @Test
+  public void test2 ()
+  {
+    final String schematron = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+                              "<sch:schema xmlns:sch=\"http://purl.oclc.org/dsdl/schematron\" queryBinding=\"xslt2\">\n" +
+                              "  <sch:title>Schematron 1</sch:title>\n" +
+                              "  <sch:pattern>\n" +
+                              "    <sch:rule context=\"shortdesc\">\n" +
+                              "        <sch:let name=\"characters\" value=\"string-length(.)\"/>\n" +
+                              "        <sch:assert test=\"$characters &lt; 10\"> \n" +
+                              "        You have characters. Short Description characters should be less than 10.       \n" +
+                              "        </sch:assert>  \n" +
+                              "    </sch:rule>\n" +
+                              "  </sch:pattern>\n" +
+                              "</sch:schema>";
+
+    final ISchematronResource aResSCHCheck1 = SchematronResourceSCH.fromString (schematron, StandardCharsets.UTF_8);
+
+    final boolean ans1 = aResSCHCheck1.isValidSchematron ();
+    assertTrue (ans1);
+
+    final String xmlFile = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+                           "<topic id=\"id168TG0I0RYF\">\n" +
+                           "  <title><b>Hi Legal done</b></title>\n" +
+                           "  <shortdesc>Content is provided for demonstration purposes only. <ph audience=\"Administrator\">Administrators and operators must read the manual before operating a new vehicle. </ph> <ph audience=\"EndUser\">Any user must read the manual before operating a new vehicle. </ph> <ph product=\"ProductA\">Your luxuriously appointed way to travel to the stars awaits you! </ph> <ph product=\"ProductB\">Your well appointed spaceship awaits you! </ph></shortdesc>\n" +
+                           "  <prolog>\n" +
+                           "  </prolog>\n" +
+                           "  <body>\n" +
+                           "    <p></p>\n" +
+                           "    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>\n" +
+                           "  </body>\n" +
+                           "</topic>";
+    final SchematronOutputType svrl;
+    try
+    {
+      svrl = aResSCHCheck1.applySchematronValidationToSVRL (new StringStreamSource (xmlFile));
+    }
+    catch (final Exception e)
+    {
+      throw new RuntimeException (e);
+    }
+    assertNotNull (svrl);
+    final List <Object> failedAsserts = svrl.getActivePatternAndFiredRuleAndFailedAssert ();
+    assertNotNull (failedAsserts);
   }
 }
