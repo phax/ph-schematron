3cbfc9eb058384a4abea95ed972371bd6db153f7
Philip Helger <philip@helger.com>
2023-05-31 14:50:57 +0200

    Added test for #143

---
+++ ph-schematron-pure/src/test/java/com/helger/schematron/pure/supplementary/Issue143Test.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2014-2023 Philip Helger (www.helger.com)
+ * philip[at]helger[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.helger.schematron.pure.supplementary;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.io.File;
+
+import javax.annotation.Nonnull;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.helger.commons.io.resource.FileSystemResource;
+import com.helger.schematron.pure.SchematronResourcePure;
+import com.helger.schematron.pure.errorhandler.LoggingPSErrorHandler;
+import com.helger.schematron.svrl.SVRLHelper;
+import com.helger.schematron.svrl.SVRLMarshaller;
+import com.helger.schematron.svrl.jaxb.SchematronOutputType;
+
+public final class Issue143Test
+{
+  private static final Logger LOGGER = LoggerFactory.getLogger (Issue143Test.class);
+
+  public static void validateAndProduceSVRL (@Nonnull final File aSchematron, final File aXML) throws Exception
+  {
+    final SchematronResourcePure aSCH = SchematronResourcePure.fromFile (aSchematron);
+    aSCH.validateCompletely (new LoggingPSErrorHandler ());
+
+    // Perform validation
+    final SchematronOutputType aSVRL = aSCH.applySchematronValidationToSVRL (new FileSystemResource (aXML));
+    assertNotNull (aSVRL);
+    if (true)
+      LOGGER.info (new SVRLMarshaller ().getAsString (aSVRL));
+
+    // XXX Should be 1 but is currently 0
+    assertEquals (1, SVRLHelper.getAllFailedAssertions (aSVRL).size ());
+  }
+
+  @Test
+  @Ignore
+  public void testIssue () throws Exception
+  {
+    validateAndProduceSVRL (new File ("src/test/resources/external/issues/github143/schematron.sch"),
+                            new File ("src/test/resources/external/issues/github143/test.xml"));
+  }
+}
