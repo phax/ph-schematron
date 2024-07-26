6f963b3c884af509bfeca2ef0c116bee5340e8b4
Philip Helger <philip@helger.com>
2024-07-26 11:46:54 +0200

    Fixed oackage name

--- ph-schematron-xslt/src/test/java/com/helger/schematron/supplementary/documentation/DocumentationExamples.java
+++ ph-schematron-xslt/src/test/java/com/helger/schematron/supplementary/documentation/DocumentationExamples.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.helger.schematron.supplementrary.documentation;
+package com.helger.schematron.supplementary.documentation;
 
 import java.io.File;
 
