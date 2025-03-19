dde1a25f90dbc8872dbc93b33175881aeed404be
Philip Helger <philip@helger.com>
2025-03-19 14:53:38 +0100

    Added S9 API test

--- ph-schematron-pure/src/test/java/com/helger/schematron/pure/supplementary/Issue185NodeSetIssueTest.java
+++ ph-schematron-pure/src/test/java/com/helger/schematron/pure/supplementary/Issue185NodeSetIssueTest.java
@@ -16,6 +16,12 @@
  */
 package com.helger.schematron.pure.supplementary;
 
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathFactory;
@@ -23,6 +29,18 @@ import javax.xml.xpath.XPathFactory;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.helger.commons.collection.impl.CommonsArrayList;
+
+import net.sf.saxon.s9api.DocumentBuilder;
+import net.sf.saxon.s9api.Processor;
+import net.sf.saxon.s9api.SaxonApiException;
+import net.sf.saxon.s9api.XPathCompiler;
+import net.sf.saxon.s9api.XPathExecutable;
+import net.sf.saxon.s9api.XPathSelector;
+import net.sf.saxon.s9api.XdmItem;
+import net.sf.saxon.s9api.XdmNode;
+import net.sf.saxon.s9api.XdmValue;
 
 /**
  * Test for GitHub issue 185
@@ -46,5 +64,40 @@ public final class Issue185NodeSetIssueTest
     // Example to use it within an expression
     final Object o = aXPath.evaluate (expression, XPathConstants.NODESET);
     LOGGER.info ("Result: " + o + " - " + o.getClass ());
+  }
+
+  @Test
+  public void testS9API () throws SaxonApiException
+  {
+    // Initialize Saxon's Processor
+    // 'false' indicates non-standalone mode
+    final Processor aS9Processor = new Processor (false);
+
+    // Create an XPathCompiler
+    final XPathCompiler aXPathCompiler = aS9Processor.newXPathCompiler ();
+
+    // Create the XPath expression
+    final String sExpression = "('AA', 'BB', 'BT', 'RO-CT', 'RO-BC')";
+    final XPathExecutable aXPathExecutable = aXPathCompiler.compile (sExpression);
+
+    // Evaluate the XPath expression
+    final XPathSelector aSelector = aXPathExecutable.load ();
+
+    if (false)
+    {
+      // Load XML file
+      // Change path to your XML file
+      final File xmlFile = new File ("yourfile.xml");
+      final DocumentBuilder aS9Builder = aS9Processor.newDocumentBuilder ();
+      final XdmNode document = aS9Builder.build (xmlFile);
+      aSelector.setContextItem (document); // Set context to the XML document
+    }
+
+    // Get the result as a sequence
+    final XdmValue result = aSelector.evaluate ();
+
+    // Optionally: Convert the result into a List<String>
+    final List <String> aEvaledList = result.stream ().map (XdmItem::getStringValue).collect (Collectors.toList ());
+    assertEquals (new CommonsArrayList <> ("AA", "BB", "BT", "RO-CT", "RO-BC"), aEvaledList);
   }
 }
